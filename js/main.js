a3c0dc1bb0346e33b2043839ba78b9955b80a18d
Ahmed-Walid2 <ahmedwalid2220@gmail.com>
2023-12-18 23:34:36 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var submitBtn = document.getElementById("submitBtn");
 var deleteBtn;
 var closeBtn = document.getElementById("closeBtn");
 var bookmarkList = [];
+var regexName = /^\w{3,}(\s+\w+)*$/;
+var regexUrl = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
 
 if (localStorage.getItem("bookmarkList") != null) {
   bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
@@ -104,3 +106,23 @@ function checkSiteUrl() {
   }
   return isValid;
 }
+
+siteName.addEventListener("input", function () {
+  if (regexName.test(siteName.value)) {
+    siteName.classList.add("green");
+    siteName.classList.remove("red");
+  } else {
+    siteName.classList.add("red");
+    siteName.classList.remove("green");
+  }
+});
+
+siteUrl.addEventListener("input", function () {
+  if (regexUrl.test(siteUrl.value)) {
+    siteUrl.classList.add("green");
+    siteUrl.classList.remove("red");
+  } else {
+    siteUrl.classList.add("red");
+    siteUrl.classList.remove("green");
+  }
+});
